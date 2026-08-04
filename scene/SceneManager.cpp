7361b0fdaeb58e5c33b8fbe52f3d98e1b4d3b636
ishii-s-oknw-witz <ishii-s@oknw.witz-inc.co.jp>
2026-08-04 17:43:39 +0900

    ボトルデリバリー用のシーンの追加

--- scene/SceneManager.cpp
+++ scene/SceneManager.cpp
@@ -4,19 +4,47 @@
 // {シーンID, 目標距離, 速度, 走行エッジ, 終了色, 目標輝度, {Kp, Ki, Kd}}
 LineTraceScene lineTraceScenes[] =
 {
-    { 0,  500, 100, RunnerEdge::RightEdge, Color::None, 50, {0.2f, 0.0f, 0.2f} }, // 直線1
-    { 1,  150,  80, RunnerEdge::RightEdge, Color::None, 50, {0.6f, 0.0f, 0.4f} }, // カーブ1-1
-    { 2,  100,  70, RunnerEdge::RightEdge, Color::None, 50, {0.5f, 0.0f, 0.4f} }, // カーブ1-2
-    { 3,  150,  80, RunnerEdge::RightEdge, Color::None, 50, {0.6f, 0.0f, 0.4f} }, // カーブ1-3
-    { 4,  400, 100, RunnerEdge::RightEdge, Color::None, 50, {0.3f, 0.0f, 0.4f} }, // 直線2
-    { 5,  150,  80, RunnerEdge::RightEdge, Color::None, 50, {0.6f, 0.0f, 0.4f} }, // カーブ2-1
-    { 6,  100,  70, RunnerEdge::RightEdge, Color::None, 50, {0.5f, 0.0f, 0.4f} }, // カーブ2-2
-    { 7,  100,  80, RunnerEdge::RightEdge, Color::None, 50, {0.6f, 0.0f, 0.4f} }, // カーブ2-3
-    { 8,  300, 100, RunnerEdge::RightEdge, Color::None, 50, {0.3f, 0.0f, 0.4f} }, // 直線3
-    { 9,  400,  60, RunnerEdge::RightEdge, Color::None, 50, {0.5f, 0.0f, 0.4f} }, // カーブ3
-    {10,  900, 100, RunnerEdge::RightEdge, Color::None, 50, {0.6f, 0.0f, 0.4f} }, // 蛇行1
-    {11,  900,  80, RunnerEdge::RightEdge, Color::None, 50, {0.5f, 0.0f, 0.4f} }, // 蛇行2
-    {12, 1500, 100, RunnerEdge::RightEdge, Color::None, 50, {0.4f, 0.0f, 0.4f} }  // 直線4
+    { 0,  500, 100, RunnerEdge::RightEdge, Color::None, 50, {0.2f, 0.0f, 0.2f} }, // Lap直線1
+    { 1,  150,  80, RunnerEdge::RightEdge, Color::None, 50, {0.6f, 0.0f, 0.4f} }, // Lapカーブ1-1
+    { 2,  100,  70, RunnerEdge::RightEdge, Color::None, 50, {0.5f, 0.0f, 0.4f} }, // Lapカーブ1-2
+    { 3,  150,  80, RunnerEdge::RightEdge, Color::None, 50, {0.6f, 0.0f, 0.4f} }, // Lapカーブ1-3
+    { 4,  400, 100, RunnerEdge::RightEdge, Color::None, 50, {0.3f, 0.0f, 0.4f} }, // Lap直線2
+    { 5,  150,  80, RunnerEdge::RightEdge, Color::None, 50, {0.6f, 0.0f, 0.4f} }, // Lapカーブ2-1
+    { 6,  100,  70, RunnerEdge::RightEdge, Color::None, 50, {0.5f, 0.0f, 0.4f} }, // Lapカーブ2-2
+    { 7,  100,  80, RunnerEdge::RightEdge, Color::None, 50, {0.6f, 0.0f, 0.4f} }, // Lapカーブ2-3
+    { 8,  300, 100, RunnerEdge::RightEdge, Color::None, 50, {0.3f, 0.0f, 0.4f} }, // Lap直線3
+    { 9,  400,  60, RunnerEdge::RightEdge, Color::None, 50, {0.5f, 0.0f, 0.4f} }, // Lapカーブ3
+    {10,  900, 100, RunnerEdge::RightEdge, Color::None, 50, {0.6f, 0.0f, 0.4f} }, // Lap蛇行1
+    {11,  900,  80, RunnerEdge::RightEdge, Color::None, 50, {0.5f, 0.0f, 0.4f} }, // Lap蛇行2
+    {12, 1500, 100, RunnerEdge::RightEdge, Color::None, 50, {0.4f, 0.0f, 0.4f} }, // Lap直線4
+    {13,    0,  70, RunnerEdge::RightEdge, Color::None, 20, {0.4f, 0.0f, 0.4f} }, // Dlvボトルまで
+    {14,  600,  70, RunnerEdge::LeftEdge,  Color::None, 50, {0.6f, 0.0f, 0.4f} }, // Dlvカーブ1
+    {15,    0,  60, RunnerEdge::LeftEdge,  Color::Blue, 50, {0.6f, 0.0f, 0.4f} }, // Dlvカーブ2
+    {16, 1200, 100, RunnerEdge::LeftEdge,  Color::None, 50, {0.3f, 0.0f, 0.4f} }, // Dlv直線1
+    {17,  300,  60, RunnerEdge::LeftEdge,  Color::None, 50, {0.6f, 0.0f, 0.4f} }, // Dlvカーブ3
+    {18,    0, 100, RunnerEdge::LeftEdge,  Color::Blue, 50, {0.3f, 0.0f, 0.4f} }, // Dlv青まで
+    {19,  400, 100, RunnerEdge::RightEdge, Color::None, 50, {0.3f, 0.0f, 0.4f} }, // Dlv帰還直線1
+    {20,  300,  60, RunnerEdge::RightEdge, Color::None, 50, {0.6f, 0.0f, 0.4f} }, // Dlv帰還カーブ1
+    {21,    0, 100, RunnerEdge::RightEdge, Color::Blue, 50, {0.3f, 0.0f, 0.4f} }  // Dlv帰還青まで
+};
+
+MoveScene moveScenes[] =
+{
+    {0, 100, Direction::front,  70, Color::None, {1.0f, 0.0f, 0.0f}}, // Dlv青スルー
+    {1, 300, Direction::front, 100, Color::None, {1.0f, 0.0f, 0.0f}}, // Dlvエリアまで
+    {1, 300, Direction::back,  100, Color::None, {1.0f, 0.0f, 0.0f}}  // Dlv線まで帰還
+};
+
+TrunScene trunScenes[] =
+{
+    {0, 90, {1.0f, 0.0f, 0.0f}} //右に90°回転
+};
+
+BottleDetectScene bottleDetectScenes[] =
+{
+    {0, Color::Yellow}, //黄ボトル検知
+    {1, Color::Blue  }, //青ボトル検知
+    {2, Color::Red   }  //赤ボトル検知
 };
 
 //コンストラクタ
